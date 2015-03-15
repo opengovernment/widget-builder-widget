d239f243b9bf42dac04c6d8affb3d2f9c2c90b01
Walter McGinnis <wm@waltermcginnis.com>
2015-03-15 21:12:22 +1300

    Add clear & start again button
    * only shows when necessary

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   actions: {
     backToStart: function() {
       this.controllerFor('application').get('widget').clearValues();
+      this.controllerFor('index').set('errorMessage', null);
 
       this.transitionTo('application');
 
