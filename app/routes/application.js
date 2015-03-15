f8d61cbfac650dc3dfff075a032dc07b8a0cb5af
Walter McGinnis <wm@waltermcginnis.com>
2015-03-15 16:52:02 +1300

    Reset data when user cancels

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
+import Widget from '../models/widget';
 
 export default Ember.Route.extend({
   actions: {
     backToStart: function() {
+      this.controllerFor('application').set('widget');
+      this.controllerFor('application').set('widget', Widget.create());
+
       this.transitionTo('application');
 
       return false;
